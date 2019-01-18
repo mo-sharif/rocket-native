07a8894100d79dd5e5df5a1cacfec6558fb6c510
Mo Sharif <mo@mosh-media.com>
2019-01-18 10:38:41 -0800

    Update dependencies

--- src/native/components/Home.js
+++ src/native/components/Home.js
@@ -13,22 +13,10 @@ import {
 import PropTypes from "prop-types";
 
 import Spacer from "./Spacer";
-import Pusher from "pusher-js/react-native";
 import { Actions } from "react-native-router-flux";
 import Header from "./Header";
 
 import { Alert, Image, View } from "react-native";
-Pusher.logToConsole = true;
-
-var pusher = new Pusher("21297c2b7b9a70373e47", {
-  cluster: "us2",
-  forceTLS: true
-});
-
-var channel = pusher.subscribe("my-channel");
-channel.bind("my-event", function(data) {
-  Alert.alert(data.message);
-});
 const img_url =
   "https://raw.githubusercontent.com/Mosh-Media/rocket-native/master/src/images/app-rocket.png";
 
