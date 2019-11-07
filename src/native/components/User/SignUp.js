3556dd70755c33e7a148398c157b671844108fac
Mo Sharif <mo@mosh-media.com>
2019-11-06 20:31:18 -0800

    Create SignUp.js

---
+++ src/native/components/User/SignUp.js
@@ -0,0 +1,121 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Container, Content, Text, Form, Item, Label, Input, Button,
+} from 'native-base';
+import { Actions } from 'react-native-router-flux';
+import Messages from '../UI/Messages';
+import Header from '../UI/Header';
+import Spacer from '../UI/Spacer';
+
+class SignUp extends React.Component {
+  static propTypes = {
+    success: PropTypes.string,
+    error: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    onFormSubmit: PropTypes.func.isRequired,
+    facebookSignin: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {
+    error: null,
+    success: null,
+  }
+
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    password2: '',
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.facebookSignin = this.facebookSignin.bind(this);
+  }
+
+  handleChange = (name, val) => this.setState({ [name]: val })
+
+  handleSubmit = () => {
+    const { onFormSubmit } = this.props;
+    onFormSubmit(this.state)
+      .then(() => setTimeout(() => { Actions.pop(); Actions.login(); }, 1000))
+      .catch(() => {});
+  }
+
+  render() {
+    const { loading, error, success } = this.props;
+
+    return (
+      <Container>
+        <Content padder>
+          <Header
+            title="Welcome"
+            content="We're glad to welcome you to the community. There's only a few questions and you'll be on your way."
+          />
+
+          {error && <Messages message={error} />}
+          {success && <Messages type="success" message={success} />}
+
+          <Form>
+            <Item stackedLabel>
+              <Label>First Name</Label>
+              <Input
+                disabled={loading}
+                onChangeText={v => this.handleChange('firstName', v)}
+              />
+            </Item>
+
+            <Item stackedLabel>
+              <Label>Last Name</Label>
+              <Input
+                disabled={loading}
+                onChangeText={v => this.handleChange('lastName', v)}
+              />
+            </Item>
+
+            <Item stackedLabel>
+              <Label>Email</Label>
+              <Input
+                disabled={loading}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                onChangeText={v => this.handleChange("email", v)}
+              />
+            </Item>
+
+            <Item stackedLabel>
+              <Label>Password</Label>
+              <Input
+                disabled={loading}
+                secureTextEntry
+                onChangeText={v => this.handleChange('password', v)}
+              />
+            </Item>
+
+            <Item stackedLabel>
+              <Label>Confirm Password</Label>
+              <Input
+                disabled={loading}
+                secureTextEntry
+                onChangeText={v => this.handleChange('password2', v)}
+              />
+            </Item>
+
+            <Spacer size={20} />
+
+            <Button block onPress={this.handleSubmit} disabled={loading}>
+              <Text>{loading ? 'Loading' : 'Sign Up'}</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+export default SignUp;
