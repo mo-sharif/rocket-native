5876fa4443a798b51cfb8e10bb0e501af7b8d8a6
Mo Sharif <mo@mosh-media.com>
2019-11-06 20:30:45 -0800

    Create Footer.js

---
+++ src/web/components/UI/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+
+const Footer = () => (
+  <footer className="mt-5">
+    <Row>
+      <Col sm="12" className="text-right pt-3">
+        <p>
+          Learn More on the
+          {' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Mosh-Media/rocket-native">
+            Github Repo
+          </a>
+          {' '}
+          &nbsp; | &nbsp; Boilerplate by
+          {' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://mcnam.ee">
+            Matt Mcnamee
+          </a>
+          {' '}
+          &nbsp; | &nbsp; Developed by
+          {' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Mosh-Media">
+            Mo Sharif
+          </a>
+          .
+        </p>
+      </Col>
+    </Row>
+  </footer>
+);
+
+export default Footer;
