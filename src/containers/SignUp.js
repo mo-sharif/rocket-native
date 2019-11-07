917e14d1d4657fd7dfd86f9e80660c9dfae72527
Mo Sharif <mo@mosh-media.com>
2019-11-06 20:32:16 -0800

    Update SignUp.js

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -2,26 +2,37 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { facebookSignin } from "../actions/FacebookActions";
-import { signUp } from '../actions/member';
-
 class SignUp extends Component {
   static propTypes = {
     Layout: PropTypes.func.isRequired,
     member: PropTypes.shape({}).isRequired,
     onFormSubmit: PropTypes.func.isRequired,
-    facebookSignin: PropTypes.func.isRequired,
-    isLoading: PropTypes.bool.isRequired,
   }
 
   state = {
-    errorMessage: null,
+    error: null,
+    success: null,
+    loading: false,
   }
 
   onFormSubmit = (data) => {
     const { onFormSubmit } = this.props;
+
+    this.setState({ loading: true });
+
     return onFormSubmit(data)
-      .catch((err) => { this.setState({ errorMessage: err }); throw err; });
+      .then(() => this.setState({
+        loading: false,
+        success: 'Success - You are now a member',
+        error: null,
+      })).catch((err) => {
+        this.setState({
+          loading: false,
+          success: null,
+          error: err,
+        });
+        throw err; // To prevent transition back
+      });
   }
 
   facebookSignin = async (data) => {
@@ -30,19 +41,15 @@ class SignUp extends Component {
   }
 
   render = () => {
-    const {
-      member,
-      Layout,
-      isLoading,
-    } = this.props;
-
-    const { errorMessage } = this.state;
+    const { member, Layout } = this.props;
+    const { error, loading, success } = this.state;
 
     return (
       <Layout
+        error={error}
         member={member}
-        loading={isLoading}
-        error={errorMessage}
+        loading={loading}
+        success={success}
         onFormSubmit={this.onFormSubmit}
         facebookSignin={this.facebookSignin}
       />
@@ -52,12 +59,10 @@ class SignUp extends Component {
 
 const mapStateToProps = state => ({
   member: state.member || {},
-  isLoading: state.status.loading || false,
 });
 
-const mapDispatchToProps = {
-  onFormSubmit: signUp,
-  facebookSignin: facebookSignin
-};
+const mapDispatchToProps = dispatch => ({
+  onFormSubmit: dispatch.member.signUp,
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
