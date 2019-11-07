c8969cbe9f1ac06db56bf48ff690126d15f15465
Mo Sharif <mo@mosh-media.com>
2019-11-06 20:32:08 -0800

    Update Login.js

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,52 +2,53 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import { login } from "../actions/member";
-import { facebookSignin } from "../actions/FacebookActions";
-
 class Login extends Component {
   static propTypes = {
     Layout: PropTypes.func.isRequired,
-    locale: PropTypes.string,
     member: PropTypes.shape({}).isRequired,
     onFormSubmit: PropTypes.func.isRequired,
-    facebookSignin: PropTypes.func.isRequired,
-    isLoading: PropTypes.bool.isRequired,
-    successMessage: PropTypes.string.isRequired
-  };
-
-  static defaultProps = {
-    locale: null
-  };
+  }
 
   state = {
-    errorMessage: null
-  };
+    error: null,
+    success: null,
+    loading: false,
+  }
 
   onFormSubmit = data => {
     const { onFormSubmit } = this.props;
-    return onFormSubmit(data).catch(err => {
-      this.setState({ errorMessage: err });
-      throw err;
-    });
-  };
+
+    this.setState({ loading: true });
+
+    return onFormSubmit(data)
+      .then(() => this.setState({
+        loading: false,
+        success: 'Success - Logged in',
+        error: null,
+      })).catch((err) => {
+        this.setState({
+          loading: false,
+          success: null,
+          error: err,
+        });
+        throw err; // To prevent transition back
+      });
+  }
 
   facebookSignin = async (data) => {
     const { facebookSignin } = this.props;
     return await facebookSignin(data).catch(e => console.log(`Error: ${e}`));
   };
   render = () => {
-    const { member, locale, Layout, isLoading, successMessage } = this.props;
-
-    const { errorMessage } = this.state;
+    const { member, Layout } = this.props;
+    const { error, loading, success } = this.state;
 
     return (
       <Layout
+        error={error}
         member={member}
-        locale={locale}
-        loading={isLoading}
-        error={errorMessage}
-        success={successMessage}
+        loading={loading}
+        success={success}
         onFormSubmit={this.onFormSubmit}
         facebookSignin={this.facebookSignin}
       />
@@ -57,15 +58,11 @@ class Login extends Component {
 
 const mapStateToProps = state => ({
   member: state.member || {},
-  locale: state.locale || null,
-  isLoading: state.status.loading || false,
-  successMessage: state.status.success || ""
 });
 
-const mapDispatchToProps = {
-  onFormSubmit: login,
-  facebookSignin: facebookSignin
-};
+const mapDispatchToProps = dispatch => ({
+  onFormSubmit: dispatch.member.login,
+});
 
 export default connect(
   mapStateToProps,
