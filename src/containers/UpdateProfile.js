fe9459a6accc0bd4cb883a874cde48e6b5b26e56
Mo Sharif <mo@mosh-media.com>
2019-11-06 20:32:01 -0800

    Update UpdateProfile.js

--- src/containers/UpdateProfile.js
+++ src/containers/UpdateProfile.js
@@ -2,43 +2,46 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { updateProfile } from '../actions/member';
-
 class UpdateProfile extends Component {
   static propTypes = {
     Layout: PropTypes.func.isRequired,
     member: PropTypes.shape({}).isRequired,
     onFormSubmit: PropTypes.func.isRequired,
-    isLoading: PropTypes.bool.isRequired,
   };
 
   state = {
-    errorMessage: null,
-    successMessage: null,
+    error: null,
+    success: null,
+    loading: false,
   }
 
   onFormSubmit = (data) => {
     const { onFormSubmit } = this.props;
+
+    this.setState({ loading: true });
+
     return onFormSubmit(data)
-      .then(mes => this.setState({ successMessage: mes, errorMessage: null }))
-      .catch((err) => { this.setState({ errorMessage: err, successMessage: null }); throw err; });
+      .then(() => this.setState({
+        loading: false,
+        success: 'Success - Updated',
+        error: null,
+      })).catch(err => this.setState({
+        loading: false,
+        success: null,
+        error: err,
+      }));
   }
 
   render = () => {
-    const {
-      member,
-      Layout,
-      isLoading,
-    } = this.props;
-
-    const { successMessage, errorMessage } = this.state;
+    const { member, Layout } = this.props;
+    const { error, loading, success } = this.state;
 
     return (
       <Layout
+        error={error}
         member={member}
-        loading={isLoading}
-        error={errorMessage}
-        success={successMessage}
+        loading={loading}
+        success={success}
         onFormSubmit={this.onFormSubmit}
       />
     );
@@ -47,11 +50,10 @@ class UpdateProfile extends Component {
 
 const mapStateToProps = state => ({
   member: state.member || {},
-  isLoading: state.status.loading || false,
 });
 
-const mapDispatchToProps = {
-  onFormSubmit: updateProfile,
-};
+const mapDispatchToProps = dispatch => ({
+  onFormSubmit: dispatch.member.updateProfile,
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(UpdateProfile);
