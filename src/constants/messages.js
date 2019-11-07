8ef4654d26db08506533b2ff4c74822a32892604
Mo Sharif <mo@mosh-media.com>
2019-11-06 20:32:13 -0800

    Create messages.js

---
+++ src/constants/messages.js
@@ -0,0 +1,29 @@
+export const generalMessages = {};
+
+export const successMessages = {};
+
+export const errorMessages = {
+  // Defaults
+  default: 'Hmm, an unknown error occured',
+  timeout: 'Server Timed Out. Check your internet connection',
+  invalidJson: 'Response returned is not valid JSON',
+
+  // Firebase Related
+  invalidFirebase: 'Firebase is not connected correctly',
+
+  // Member
+  memberNotAuthd: 'You need to be logged in, to update your profile',
+  memberExists: 'Member already exists',
+  missingFirstName: 'First name is missing',
+  missingLastName: 'Last name is missing',
+  missingEmail: 'Email is missing',
+  missingPassword: 'Password is missing',
+  missingPostTitle: 'Post title is missing',
+  missingPostBody: 'Post body is missing',
+  missingimage: 'Post image is missing',
+  passwordsDontMatch: 'Passwords do not match',
+
+  // Posts
+post404: 'Post not found',
+  missingMealId: 'Missing meal definition',
+};
