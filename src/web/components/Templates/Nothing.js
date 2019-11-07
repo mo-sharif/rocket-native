2539259d17480bcd10ac02a4d4a2773138cb301e
Mo Sharif <mo@mosh-media.com>
2019-11-06 20:30:36 -0800

    Create Nothing.js

---
+++ src/web/components/Templates/Nothing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Row, Col } from 'reactstrap';
+import { Helmet } from 'react-helmet';
+
+const Template = ({ pageTitle, children }) => (
+  <Container>
+    <Helmet>
+      <title>{pageTitle}</title>
+    </Helmet>
+
+    <Row>
+      <Col sm="12">{children}</Col>
+    </Row>
+  </Container>
+);
+
+Template.propTypes = {
+  pageTitle: PropTypes.string,
+  children: PropTypes.element.isRequired,
+};
+
+Template.defaultProps = {
+  pageTitle: 'React App',
+};
+
+export default Template;
