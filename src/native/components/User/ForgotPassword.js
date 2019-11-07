0f0165d27783dca370ea0becb7c477dc88f71d1e
Mo Sharif <mo@mosh-media.com>
2019-11-06 20:30:13 -0800

    Create ForgotPassword.js

---
+++ src/native/components/User/ForgotPassword.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Container, Content, Text, Form, Item, Label, Input, Button,
+} from 'native-base';
+import { Actions } from 'react-native-router-flux';
+import Messages from '../UI/Messages';
+import Header from '../UI/Header';
+import Spacer from '../UI/Spacer';
+
+class ForgotPassword extends React.Component {
+  static propTypes = {
+    member: PropTypes.shape({
+      email: PropTypes.string,
+    }),
+    error: PropTypes.string,
+    success: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    onFormSubmit: PropTypes.func.isRequired,
+  }
+
+  static defaultProps = {
+    success: null,
+    error: null,
+    member: {},
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: (props.member && props.member.email) ? props.member.email : '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (name, val) => this.setState({ [name]: val })
+
+  handleSubmit = () => {
+    const { onFormSubmit } = this.props;
+
+    return onFormSubmit(this.state)
+      .then(() => setTimeout(() => { Actions.pop(); Actions.login(); }, 1000))
+      .catch(() => {});
+  }
+
+  render() {
+    const { loading, error, success } = this.props;
+    const { email } = this.state;
+
+    return (
+      <Container>
+        <Content padder>
+          <Header
+            title="Reset your Password"
+            content="No stress, no stress. We'll get you back into your account."
+          />
+
+          {error && <Messages message={error} />}
+          {success && <Messages type="success" message={success} />}
+
+          <Form>
+            <Item stackedLabel>
+              <Label>Email</Label>
+              <Input
+                autoCapitalize="none"
+                value={email}
+                disabled={loading}
+                keyboardType="email-address"
+                onChangeText={v => this.handleChange('email', v)}
+              />
+            </Item>
+
+            <Spacer size={20} />
+
+            <Button block onPress={this.handleSubmit} disabled={loading}>
+              <Text>{loading ? 'Loading' : 'Reset Password'}</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+export default ForgotPassword;
